9dff4c4ab74a1e2989503e0de9ec5b9f4f221b2e
hmaravlad <hmara.vladislav2@gmail.com>
2022-04-07 17:10:10 +0300

    add types to describe api config

--- src/types/service-config.ts
+++ src/types/service-config.ts
@@ -3,4 +3,56 @@ export interface ServiceConfig {
   type: string;
   lang: string;
   port: number;
+  api?: APIConfig;
+}
+
+export interface APIConfig {
+  endpointGroups: EndpointGroup[];
+}
+
+export interface EndpointGroup {
+  name: string;
+  prefix: string;
+  endpoints: Endpoint[]
+  entities: Entity[];
+}
+
+export interface Endpoint {
+  name: string;
+  path: string;
+  method: string;
+  request?: Exchange;
+  response?: Exchange
+}
+
+export interface Entity {
+  id: number;
+  name: string;
+  fields: Field[];
+}
+
+export interface Value {
+  type: string | EntityRef;
+  items?: Value;
+}
+
+export interface Field extends Value {
+  name: string;
+}
+
+export interface Exchange {
+  statusCode?: number;
+  content: Value;
+}
+
+export interface EntityRef {
+  id: number;
+}
+
+export function isEntityRef(obj: unknown): obj is EntityRef {
+  return (obj as EntityRef).id !== undefined;
+}
+
+export function isValue(obj: unknown): obj is Value {
+  return (obj as Value).type !== undefined;
 }
