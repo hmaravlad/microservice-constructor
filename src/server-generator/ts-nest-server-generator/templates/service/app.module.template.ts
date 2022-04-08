98ede5bec0c7e0bdef4dc0505ec30981623551e7
hmaravlad <hmara.vladislav2@gmail.com>
2022-04-08 16:23:17 +0300

    add module import to app.module

--- src/server-generator/ts-nest-server-generator/templates/service/app.module.template.ts
+++ src/server-generator/ts-nest-server-generator/templates/service/app.module.template.ts
@@ -1,3 +1,5 @@
+import { addIndentation } from '../../../../utils/add-indentation';
+import { Capitalize } from '../../../../utils/case-utils';
 import { File } from '../../../../types/file';
 import { FileTemplate } from '../../../../types/file-template';
 import { ServiceConfig } from '../../../../types/service-config';
@@ -10,14 +12,25 @@ export class AppModuleTsTemplate implements FileTemplate<ServiceConfig> {
       data: `
         import { Module } from '@nestjs/common';
         import { AppController } from './app.controller';
+        ${this.generateModuleImports(config)}
 
         @Module({
-          imports: [],
+          imports: [${config.api ? config.api.endpointGroups.map(({ name }) => `${Capitalize(name)}Module`) : ''}],
           controllers: [AppController],
           providers: [],
         })
         export class AppModule {}      
       `,
     };
+  }
+
+  generateModuleImports(config: ServiceConfig): string {
+    if (!config.api) return '';
+
+    const imports = config.api.endpointGroups
+      .map(({ name }) => `import { ${Capitalize(name)}Module } from './${name}/${name}.module';` )
+      .join('\n');
+
+    return addIndentation(imports, '\t\t\t\t', true);
   }
 }
